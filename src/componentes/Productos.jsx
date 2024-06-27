c76d58423526bab83cbb3e3ce170caf2cb76df4a
Daniel Caballero <danielcaballero3960@gmail.com>
2024-06-27 02:29:23 -0300

    clase 7

--- src/componentes/Productos.jsx
+++ src/componentes/Productos.jsx
@@ -1,8 +1,24 @@
 
 
-function Productos(){
+function Productos({productos}){
     return(
-        <div>Productos</div>
+        <section className="gridProductos">
+
+            {
+                productos.map((producto,i)=> {
+                    return  (
+
+                        <article key={i} className="cardProducto">
+                            <h2>{producto.nombre}</h2>
+                            <p>${producto.precio}</p>
+                            <h4>{producto.daño}</h4>
+                        </article>
+
+                    )
+                })
+            }
+
+        </section>
     )
 }
 
--- src/componentes/ProductosContainer.jsx
+++ src/componentes/ProductosContainer.jsx
@@ -6,24 +6,39 @@ import { useState } from "react"
 
 function ProductosContainer(){
 
+    const [productos, setProductos] = useState([])
+    const [pagina, setPagina] = useState(1)
 
     useEffect(() => {
-
-        const [estado, setEstado] = useState()
 
         fetch("/productos.json")
         .then((res) =>  {
             return res.json()
         })
         .then((data) => {
-            console.log(data)
-            setEstado(data)
+            setProductos(data)
         })
+
     },[])
+
+    const cambiarPaginaUno = () => {
+        setPagina(1)
+      }
+    const cambiarPaginaDos = () => {
+        setPagina(2)
+      }
+    const cambiarPaginaTres = () => {
+        setPagina(3)
+      }
 
 
     return(
-        <Productos></Productos>
+        <div>
+            <Productos productos={productos}/>
+            <button onClick={cambiarPaginaUno}>Pagina 1</button>
+            <button onClick={cambiarPaginaDos}>Pagina 2</button>
+            <button onClick={cambiarPaginaTres}>Pagina 3</button>
+     </div>
     )
 }
 
